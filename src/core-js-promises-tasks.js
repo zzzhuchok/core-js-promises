04cd2a3b76e8cd7938a777e5311eb873b498ed5a
zzzhuchok <zzzhuchok@gmail.com>
2025-04-14 08:40:46 +0300

    feat: add a solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -81,8 +81,10 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises)
+    .then((res) => res)
+    .catch((res) => res);
 }
 
 /**
